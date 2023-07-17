f713f365d602681188a287552ba6e54de670e2a1
heejung-newheee <aldls2311@gmail.com>
2023-07-17 20:59:23 +0900

    Create : first commit

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StButton, StButtonWrap } from '../components/common/Button';
 
 const Join = () => {
   return (
@@ -7,12 +8,21 @@ const Join = () => {
       <form action="">
         <label htmlFor="">이름</label>
         <input type="text" />
-        <label htmlFor="">이름</label>
+        <label htmlFor="">닉네임</label>
         <input type="text" />
-        <label htmlFor="">이름</label>
+        <label htmlFor="">이메일</label>
         <input type="text" />
-        <label htmlFor="">이름</label>
+        <label htmlFor="">비밀번호</label>
         <input type="text" />
+        <label htmlFor="">비밀번호 확인</label>
+        <input type="text" />
+
+        <label htmlFor="checkbox">관리자로 회원가입</label>
+        <input type="checkbox" id="checkbox" />
+        <StButtonWrap>
+          <StButton disabled>회원가입</StButton>
+          <StButton>홈으로 돌아가기</StButton>
+        </StButtonWrap>
       </form>
     </>
   );
