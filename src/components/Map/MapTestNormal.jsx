1a0ec6a8eaf0f6e121856a518c075ae523df9213
Sunwoo <dlatjsdn205@Naver.com>
2023-07-19 12:05:26 +0900

    Chore: test

--- src/components/Map/MapTestNormal.jsx
+++ src/components/Map/MapTestNormal.jsx
@@ -1,42 +1,37 @@
 import React, { useEffect, useRef, useState } from 'react';
 const { kakao } = window;
+
 const Map = () => {
   const mapRef = useRef(null);
   const markerRef = useRef(null);
+  const overlayRef = useRef(null);
   const [map, setMap] = useState(null);
   const [markerInfo, setMarkerInfo] = useState(null);
   const [searchError, setSearchError] = useState(false);
 
   useEffect(() => {
-    //맵을 보여줄 html요소에 Ref를 부여해 DOM 요소를 가져온다.
     const container = mapRef.current;
-    //지도를 생성할 때 필요한 기본 옵션 (대한민국 지도의 중앙 위치, 제주를 제외한 한반도 다 보여주기)
     const options = {
       center: new kakao.maps.LatLng(36.469287, 128.0803538),
-      level: 13, // 확대/축소 레벨
+      level: 13,
     };
 
-    //지도 생성 및 객체 리턴
     const newMap = new kakao.maps.Map(container, options);
-    // 지도를 클릭했을 때 이벤트 핸들러(핀생성)
     const mapClickHandler = mouseEvent => {
-      // 클릭한 위도, 경도 정보를 가져옵니다
       const position = mouseEvent.latLng;
-      // 이미 마커가 있는 경우 초기화
       if (markerRef.current) {
         markerRef.current.setMap(null);
       }
-      //마커 위치 최신화
       markerRef.current = new kakao.maps.Marker({
         position,
       });
       markerRef.current.setMap(newMap);
-      // 주소-좌표 변환 객체를 생성
       const geocoder = new kakao.maps.services.Geocoder();
       geocoder.coord2Address(position.getLng(), position.getLat(), (result, status) => {
         if (status === kakao.maps.services.Status.OK) {
           const address = result[0].address.address_name;
           setMarkerInfo({ position, address });
+          showCustomOverlay(newMap, position, address);
         }
       });
     };
@@ -50,9 +45,29 @@ const Map = () => {
     setMap(newMap);
   }, []);
 
-  // 검색 이벤트 핸들러
+  const showCustomOverlay = (map, position, text) => {
+    if (overlayRef.current) {
+      overlayRef.current.setMap(null);
+      overlayRef.current = null;
+    }
+
+    const content = `
+      <div style="position: absolute; padding: 5px; background-color: #fff; font-size: 12px;">
+        ${text}
+      </div>
+    `;
+
+    const customOverlay = new kakao.maps.CustomOverlay({
+      content,
+      position,
+      yAnchor: 1,
+    });
+
+    overlayRef.current = customOverlay;
+    customOverlay.setMap(map);
+  };
+
   const searchHandler = () => {
-    //입력한 값 가져옴 // state로 변경 필요
     const keyword = document.getElementById('search-input').value;
     const ps = new kakao.maps.services.Places();
     ps.keywordSearch(keyword, (data, status) => {
@@ -62,18 +77,38 @@ const Map = () => {
         if (markerRef.current) {
           markerRef.current.setMap(null);
         }
-        // 가져온 장소들의 좌표를 저장
+
+        if (overlayRef.current) {
+          overlayRef.current.setMap(null);
+          overlayRef.current = null;
+        }
+
         data.forEach(place => {
           const position = new kakao.maps.LatLng(place.y, place.x);
           bounds.extend(position);
+          const marker = new kakao.maps.Marker({
+            position: position,
+          });
+          marker.setMap(map);
+          kakao.maps.event.addListener(marker, 'click', () => {
+            const address = place.address_name;
+            setMarkerInfo({ position, address });
+            showCustomOverlay(map, position, address);
+          });
         });
-        // 좌표들 기준으로 지도 설정
+
         map.setBounds(bounds);
-        setSearchError(false); // 검색 에러 초기화
+        setSearchError(false);
       } else {
-        setSearchError(true); // 검색 에러 표시
+        setSearchError(true);
+
         if (markerRef.current) {
           markerRef.current.setMap(null);
+        }
+
+        if (overlayRef.current) {
+          overlayRef.current.setMap(null);
+          overlayRef.current = null;
         }
       }
     });
